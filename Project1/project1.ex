0be3e07f8d26ac1ab61e2df820a25d9e7058b695
Shreyas Gaadikere Sreedhara <shreyasgaadikere@ufl.edu>
2018-09-22 19:28:06 -0400

    Initial commit
    Including readme and the first project

---
+++ Project1/project1.ex
@@ -0,0 +1,66 @@
+# 
+# Nirali Patel (36318593), Shreyas Gaadikere Sreedhara (13144396)
+# University of Florida
+#
+# Project 1 - Given n and k, calculate the sum of sqaures of numbers starting from 1 till n for k consecutive numbers and 
+#            return only such an n for which the sum of squares is a perfect square
+#
+#            Ex1: n = 3, k = 2
+#                Output: 3
+#
+#            Ex2: n = 40, k = 24
+#                Output: 1, 9, 20, 25
+#
+
+
+defmodule Project1 do
+    def start do
+        n = String.to_integer(Enum.at(System.argv,0))                       # Store n from the args_tuple
+        k = String.to_integer(Enum.at(System.argv,1))                       # Store k from the args_tuple
+        no_of_cores = 10*System.schedulers_online                           # Total number of cores on the system
+        work_range = n / no_of_cores |> Float.ceil |> Kernel.trunc          # number of numbers that has to be processed by each thread 
+        t = 1                                                               # Initial number
+        if n < no_of_cores do                                               # Run if n is less than number of cores
+            create_actors(1, t, n, k, n, self())
+            receive do
+                {:ok, _} -> nil
+            end
+        else                                                                # Run if n is more than the number of cores
+           create_actors(no_of_cores, t, work_range, k, n, self())          # Recursive function to create actors 
+           for _ <- 1..no_of_cores do
+                receive do
+                    {:ok, _} -> nil
+                end
+            end
+        end
+    end
+
+    def create_actors(no_of_cores, t, work_range, k, n, pid) when no_of_cores <= 1 do        # Only last actor spawning function
+        spawn(Project1, :worker, [t, n, k, n, pid])
+    end
+
+    def create_actors(no_of_cores, t, work_range, k, n, pid) do                              # Actor spawning function
+        spawn(Project1, :worker, [t, t + work_range, k, n, pid])
+        create_actors(no_of_cores - 1, t + work_range + 1, work_range, k, n, pid)
+    end
+
+    def worker(start_num, end_num, k, n, pid) do
+        for x <- start_num..end_num do
+            y = x + k - 1
+            sum1 = (y *(y+1) *((2*y)+1))                                    # sum of squares starting from one to range
+            sum = div(sum1,6)
+            un = x-1
+            un_sum = (un * (un+1) * ((2*un)+1))                             # sum of squares starting from one to starting number-1
+            unwanted_sum = div(un_sum,6)
+            real_sum =  sum - unwanted_sum                                  # sum of squares for required range
+            sqr_root = :math.sqrt(real_sum)                                 # root of the number to see if it is a perfect square
+            round_sqrt = Float.floor(sqr_root)                   
+            if sqr_root - round_sqrt == 0 do                                # if the number is perfect square then print it
+                IO.puts "#{x}"
+            end
+        end
+        send pid, {:ok, "done"}                                             # Send message back to the boss
+    end
+end
+
+Project1.start()
--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Distributed Operating System Principles
 
-Projects I did as a part of my DOS course
+All devices from phones to supercomputers are connected to the Internet or other communication networks. On the other hand, we have problems that are too large for any one device. So, we need to involve multiple devices/machines to solve such problems. Distributed systems principles and methods are crucial for tackling such a task. Networking provides the communication infrastructure, Algorithms/Data-structures/Databases the computational techniques but Distributed Systems provide the glue that holds everything together under mildly or harshly adversarial conditions. This class will be focused on tackling the challenges that arise in a principled manner.
+
+Course Outline
+
+1. Actor model and Elixir
+2. Definition, goals and examples of Distributed Systems
+3. System architectures and models
+4. Inter process communication
+5. Remote invocation, indirect communication
+6. Operating system support
+7. Distributed objects, web services and peer to peer systems
+8. Distributed file systems
+9. Time and global states
+10. Coordination and agreement
+11. Transaction and concurreny control
+12. Distributed transactions
+13. Replication
+14. Distributed multimedia systems 
+15. Security, access control
+
+Author information:
+
+1. Shreyas Gaadikere Sreedhara, Email - shreyasgaadikere@ufl.edu
+2. Nirali Patel, Email - niralipatel@ufl.edu
